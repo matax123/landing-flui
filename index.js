b9d5c9aa5e14a2e89bf16b7d0c567beb79a63944
matax123 <matias.ortiz@alumnos.ucn.cl>
2023-07-21 15:41:39 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+new WOW().init()
+
+window.onload = function(){
+    $("#loading").fadeOut(1000);
+    $("body").removeClass("preload");
+    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+    var mobileURL = "https://api.whatsapp.com/send/?phone=56991353540&text=";
+    var pcURL = "https://web.whatsapp.com/send?phone=56991353540&text=";
+
+    if (isMobile) {
+        document.getElementById("telefono").href = mobileURL;
+    } else {
+        document.getElementById("telefono").href = pcURL;
+    }
+}
